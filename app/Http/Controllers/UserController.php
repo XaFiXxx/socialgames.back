4f09fa8b20548b30c90dd1d6a1bb9f2bd7d5c626
Benjamin Schroeders <benjamin.schroeders@icloud.com>
2024-04-21 00:23:25 +0200

    tri des posts "created_at" DESC

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,15 +9,19 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function userProfile($id)
-{
-    try {
-        $user = User::with(['games', 'posts', 'friends', 'platforms'])->findOrFail($id);
-        $userData = $user->toArray();
-        unset($userData['is_admin']);  // Supprimez les clés sensibles
-        return response()->json($userData);
-    } catch (\Exception $e) {
-        return response()->json(['error' => 'User not found'], 404);
+    {
+        try {
+            $user = User::with(['games', 'friends', 'platforms', 'posts' => function($query) {
+                $query->orderBy('created_at', 'desc');
+            }])->findOrFail($id);
+    
+            $userData = $user->toArray();
+            unset($userData['is_admin']);  // Supprimez les clés sensibles
+            return response()->json($userData);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
     }
-}
+    
 
 }
