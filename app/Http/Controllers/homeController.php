48990c699be5b68b53ef9a50ae75c9dfe52add1b
Benjamin Schroeders <benjamin.schroeders@icloud.com>
2024-06-06 01:30:01 +0200

    ajout pseudo user à un post

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -11,30 +11,30 @@ use Illuminate\Http\Request;
 class homeController extends Controller
 {
     public function home()
-{
-    // Récupérer l'utilisateur authentifié
-    $user = auth()->user();
+    {
+        // Récupérer l'utilisateur authentifié
+        $user = auth()->user();
 
-    // Récupérer les posts ordonnés par date de création en ordre décroissant
-    $posts = Post::orderBy('created_at', 'desc')
-                ->with('likes', 'comments.user') // Charger les likes des posts
-                ->get()
-                ->map(function($post) use ($user) {
-                    // Vérifier si le post est liké par l'utilisateur courant
-                    $post->is_liked = $post->likes->contains('user_id', $user->id);
-                    $post->likes_count = $post->likes->count();
-                    return $post;
-                });
+        // Récupérer les posts ordonnés par date de création en ordre décroissant
+        $posts = Post::orderBy('created_at', 'desc')
+                    ->with('user', 'likes', 'comments.user') // Charger les likes et l'utilisateur qui a créé le post
+                    ->get()
+                    ->map(function($post) use ($user) {
+                        // Vérifier si le post est liké par l'utilisateur courant
+                        $post->is_liked = $post->likes->contains('user_id', $user->id);
+                        $post->likes_count = $post->likes->count();
+                        return $post;
+                    });
 
-    // Récupérer les jeux ordonnés par date de création en ordre décroissant
-    $games = Game::orderBy('created_at', 'desc')->get();
+        // Récupérer les jeux ordonnés par date de création en ordre décroissant
+        $games = Game::orderBy('created_at', 'desc')->get();
 
-    // Renvoie les posts et les jeux en JSON sous des clés distinctes
-    return response()->json([
-        'posts' => $posts,
-        'games' => $games
-    ]);
-}
+        // Renvoie les posts et les jeux en JSON sous des clés distinctes
+        return response()->json([
+            'posts' => $posts,
+            'games' => $games
+        ]);
+    }
 
 
 }
